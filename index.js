1fb80691dee74df0eb10102ce82700ccb81191af
collins muriuki <muriukicollins48@gmail.com>
2025-03-04 08:30:29 +0300

    added new functions in index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;  
+}
+
+console.log(add(400, 470));  
+
+
+function subtract(x,y) {
+    return x-y;
+}
+console.log(subtract(844,422));
+function multiply(x,y) {
+    return x*y;
+}
+console.log(multiply(200, 119.32));  
+function divide(x,y) {
+    return x/y;
+}
+console.log(divide(5, 7));
+
+function increment(n) {
+    return n+1;
+}
+console.log(increment(827));
+function decrement(m) {
+    return m-1;
+}
+console.log(decrement(522));
+function makeInt(n) {
+    return parseInt(n, 10); 
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n); 
+  }
+  
